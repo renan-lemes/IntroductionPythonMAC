deed48d568fe02917a78f0995bf48054e05fde7c
RenanLemes <renan_ll@hotmail.com>
2022-11-30 10:39:53 -0300

    Chores:Desenvolvendo  mais exercios

--- Exercicios.py
+++ Exercicios.py
@@ -1,37 +1,3 @@
-''' 
-    Exercicio 1
-    Utilize o turtle para desenhar um quadrado e um circulo.
-    Solução:
-'''
 
-''' 
-    Exercicio 2
-    Usando recursividade contrua uma função chamada de fac em que
-    ela retorne o fatorial de um numero inteiro.
-    Para veriticar se etá correto use o math.factorial para comparar.
-    Solução:
-'''
 
-''' 
-    Exercicio 3
-    Programe a série de Leibniz para poder determinar pi.
-    Para verificar se está certo use math.pi pra ver a resposta.
-    Solução:
-'''
 
-''' 
-    Exercicio 4
-
-    Solução:
-'''
-
-''' 
-    Exercicio 5
-
-    Solução:
-'''
-
-''' 
-    DESAFIO:
-    
-'''
