e51ce625c6db8ebbcf8d9ebda34d394e74f30cbb
RenanLemes <renan_ll@hotmail.com>
2022-12-04 16:23:54 -0300

    Add:finalizando o exercicio 11

--- Solucoes.py
+++ Solucoes.py
@@ -148,3 +148,30 @@
 #         return (fibon(n-1) + fibon(n-2))
     
 # print(fibon(7))
+
+# 11
+
+# precisa arrumar a questão de letras voltar quando o range utrapassa.
+
+# letras = 'abcdefghijklmnopqrstuvxwyz'
+
+# palavra = 'palavrasenha'
+
+
+# cripto = []
+
+# senha = ''
+
+# base = 3
+
+# #print(letras.find('a'))
+
+# for i in range(len(palavra)):
+
+#     #if ((letras.find(palavra[i]) + 3) > 25 ):
+#     cripto.append(letras[letras.find(palavra[i]) + 3])
+
+# for i in range(len(cripto)):
+#     senha += cripto[i]
+
+# print(senha)
